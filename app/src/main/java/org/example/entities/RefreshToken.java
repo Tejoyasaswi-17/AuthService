156138357738e219e4ef540970314dd1c8d52590
Tejoyasaswi-17 <tejoyasaswi1703@gmail.com>
2025-02-08 19:13:48 +0530

    built the entities

--- app/src/main/java/org/example/entities/RefreshToken.java
+++ app/src/main/java/org/example/entities/RefreshToken.java
@@ -1,10 +1,19 @@
 package org.example.entities;
 
 
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.annotation.*;
-import jakarta.persistence.*;
-import lombok.*;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.*;
 
--- app/src/main/java/org/example/entities/UserInfo.java
+++ app/src/main/java/org/example/entities/UserInfo.java
@@ -1,10 +1,17 @@
 package org.example.entities;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinTable;
 
 import java.util.*;
 
